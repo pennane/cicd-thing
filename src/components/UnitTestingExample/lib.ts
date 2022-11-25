8d7a6c241582c2eb80e3592b5b6963150e68f333
Arttu Pennanen <arttu@pennanen.org>
2022-11-25 03:07:15 +0200

    chore: force failing test

--- src/components/UnitTestingExample/lib.ts
+++ src/components/UnitTestingExample/lib.ts
@@ -1,2 +1,2 @@
 export const stringToNumber = (s: string) => parseInt(s)
-export const getTotalPrice = (price = 0, count = 0) => price * count
+export const getTotalPrice = (price = 0, count = 0) => price * count * 0.5
